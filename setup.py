b4702c89fef4ce7e9897bca9075a41862de86492
David Wallis <David.Wallis@transunion.com>
2022-10-20 11:55:59 +0100

    updates

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version='0.0.1',
     author='davidwallis',
     author_email='david@wallis2000.co.uk',
-    packages=['pyHyperV'],
+    packages=['pyOrchestrator'],
     url='https://github.com/davidwallis3101/pyOrchestrator',
     license='Apache License',
-    description='A simple client for calling System Center Orchestator runbooks in python',
+    description='A simple client for calling System Center Orchestrator runbooks in python',
     long_description=open('README.rst').read(),
     install_requires=[
         "requests >= 2.0.0",
